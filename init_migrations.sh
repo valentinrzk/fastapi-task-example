4e36a6dc306256253f167ce4d8e2236019b05ceb
Valentin <dyeablaeparse@gmail.com>
2025-09-04 21:54:52 +0500

    fix: the app works now

---
+++ init_migrations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ждём 30 секунд
+echo "Waiting for 30 seconds..."
+sleep 30
+
+# Создаём ревизию Alembic с автогенерацией
+echo "Creating initial Alembic revision..."
+alembic revision --autogenerate -m "Initial revision"
+
+# Применяем все миграции
+echo "Upgrading database to latest revision..."
+alembic upgrade head
+
+echo "Migrations completed."
